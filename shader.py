49b551b6d39cc41fbb86c8ac5195b0499471cc70
uselesss <47245582+uselesss@users.noreply.github.com>
2021-03-19 22:03:19 +0300

    Shader class and vertex shader

---
+++ shader.py
@@ -0,0 +1,38 @@
+from arcade import get_window
+from arcade.gl import geometry
+
+class Shader:
+    def __init__(self):
+        self.window = get_window()
+        if not self.window:
+            raise RuntimeError("No window found")
+
+        self.ctx = self.window.ctx
+        self.mouse_pos = 0, 0
+        self.quad = geometry.quad_2d_fs()
+
+        vertex   = open('vertex_shader.glsl', 'r').read()
+        fragment = open('fragment_shader.glsl', 'r').read()
+
+        self.program = self.ctx.program(
+            vertex_shader=vertex,
+            fragment_shader=fragment
+        )
+        
+
+    def draw(self, time: float = 0, target=None):
+        try:
+            self.program['iTime'] = time
+        except KeyError:
+            pass
+        try:
+            self.program['iMouse'] = self.mouse_pos[0], -self.mouse_pos[1]
+        except KeyError:
+            pass
+        try:
+            if self.window is not None:
+                self.program['iResolution'] = self.window.get_framebuffer_size()
+        except KeyError:
+            pass
+
+        self.quad.render(self.program)
