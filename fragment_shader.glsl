3ef291f2bc2bd34885e437d7556121ab9f2f6991
uselesss <47245582+uselesss@users.noreply.github.com>
2021-03-19 22:01:40 +0300

    major changes

---
+++ fragment_shader.glsl
@@ -0,0 +1,90 @@
+#version 330
+
+uniform sampler2D background;
+uniform float iTime;
+uniform vec2 iMouse;
+uniform vec2 iResolution;
+
+in vec2 v_uv;
+out vec4 out_color;
+
+float getMu (int i, float abszSquared) {
+    return i + 1 - (log(0.5 * log(abszSquared))) / log(2.0);
+}
+
+float mandelbrot(vec2 c, int iters) {
+    vec2 z = vec2(0.0, 0.0);
+    for (int i=0; i < iters; i++) {
+        z = vec2(z.x * z.x - z.y * z.y + c.x, 2 * z.x * z.y + c.y);
+
+        float abszSquared = z.x * z.x + z.y * z.y;
+        float mu = getMu(i, abszSquared);
+        if (abszSquared > 4.0) return mu;
+    }
+    return iters;
+}
+
+float burningShip(vec2 c, int iters) {
+    vec2 z = vec2(0, 0);
+    for (int i=0; i<iters; i++) {
+        z = vec2(z.x * z.x - z.y * z.y + c.x, 2 * abs(z.x) * abs(z.y) + c.y);
+
+        float abszSquared = z.x * z.x + z.y * z.y;
+        float mu = getMu(i, abszSquared);
+
+        if (abszSquared > 16.0) return mu;
+    }
+    return iters;
+}
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    int iters = 500;
+    float maxTime = 15.0;
+    float diameter = 2.5;
+
+    float speed = 8;
+
+    vec3 col;
+    vec2 scaled;
+
+    vec2 uv = fragCoord/iResolution.xy; 
+    
+   
+    float t = iTime + 1; //min(iTime+1, maxTime);
+    scaled.x =  -1.74998 + (uv.x - 0.5) / (pow(t, (t/speed))); 
+    scaled.y =  -0.015 - (uv.y - 0.5) / (pow(t, (t/speed))); 
+    
+
+    float i = mandelbrot(vec2(3.5 * uv.x - diameter, 2.5*uv.y-1.25), iters);
+    //float i = burningShip(vec2(3.5 * uv.x - diameter, 1 - 2*uv.y), iters);
+    //float i = burningShip(scaled, iters);
+    //loat i = mandelbrot(scaled, iters);
+
+
+    float p = 32;
+    float offset = 0;
+    
+    float c = sin(p * i/float(iters) + offset)/2 + 1;
+
+    if (i == float(iters)) col = vec3(0, 0, 0);
+    else col = hsv2rgb(vec3(c, 1.0, 1.0));
+    
+
+    
+
+    fragColor = vec4(col, 1.0);
+}
+
+void main() {
+    vec4 color;
+    mainImage(color, gl_FragCoord.xy);
+    out_color = color;
+}
