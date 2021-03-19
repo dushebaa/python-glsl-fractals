6bded2dee524ce951f211f5fcc983417212359b9
uselesss <47245582+uselesss@users.noreply.github.com>
2021-03-19 22:02:38 +0300

    shader class import

--- fractals.py
+++ fractals.py
@@ -1,31 +1,28 @@
 import arcade
-from arcade.experimental.shadertoy import ShaderToy
+from shader import Shader
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "window"
+SCREEN_TITLE = "Fractal Plotter"
 
-shader = open('shader.glsl', 'r').read()
-
-
-class MyGame(arcade.Window):
+class Window(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title, resizable=True)
         self.time = 0
-        self.shadertoy = ShaderToy(shader)
-        self.set_vsync = True
+        self.shader = Shader()
+        self.set_update_rate = 1/60;
 
     def on_draw(self):
         arcade.start_render()
-        self.shadertoy.draw(time=self.time)
+        self.shader.draw(time=self.time)
 
     def on_update(self, dt):
         self.time += dt
 
     def on_mouse_drag(self, x: float, y: float, dx: float, dy: float, _buttons: int, _modifiers: int):
-        self.shadertoy.mouse_pos = x, y
-
+       self.shader.mouse_pos = x, y
+    
 
 if __name__ == "__main__":
-    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
